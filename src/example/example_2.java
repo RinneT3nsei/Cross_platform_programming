2f445dfad418935b862bfae93d09608ab14ffa59
RinneT3nsei <58113647+RinneT3nsei@users.noreply.github.com>
2022-10-18 14:32:39 +0300

    Добавил(-а) файлы по лабораторной №2

---
+++ src/example/example_2.java
@@ -0,0 +1,14 @@
+package example;
+
+public class example_2 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            System.out.println("1");
+            throw new RuntimeException("Неперевірена помилка");
+        } catch (Exception e) {
+            System.out.println("2" + e);
+        }
+        System.out.println("3");
+    }
+}
